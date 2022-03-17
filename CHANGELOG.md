7bf9780ff32536a3860b6267272296d46e7430cc
Tam <Tam@users.noreply.github.com>
2022-03-17 16:41:21 +0000

    Fix issue when settings are uninitialized
    Add Changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+## 0.0.2 - 2022-03-17
+### Fixed
+- Fix issue when settings are uninitialized
+
+## 0.0.1 - 2022-03-17
+- Initial Release
--- src/Settings.php
+++ src/Settings.php
@@ -7,7 +7,7 @@ use craft\base\Model;
 class Settings extends Model
 {
 
-	public string $accessTokenId;
-	public string $secretKey;
+	public string $accessTokenId = '';
+	public string $secretKey = '';
 
 }
